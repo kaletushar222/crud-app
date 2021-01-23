3afaea30d8fc5479cbc391e06ddd724ec1ccc283
tushar_kale <tushar@procmart.com>
2021-01-24 00:56:34 +0530

    project setup

---
+++ src/client/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ComponentHome extends Component {
+    render() {
+        return (
+            <div>
+                tushar
+            </div>
+        )
+    }
+}
+export default ComponentHome;
