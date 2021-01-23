b89f31ce3cd3d3cd62e16fc1c150a4deca65a10a
tushar_kale <tushar@procmart.com>
2021-01-24 01:33:54 +0530

    Home page

--- src/client/Home.js
+++ src/client/Home.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { Table, Container, Button } from 'react-bootstrap';
+import { employees } from '../static_data/employee_data'
 
 class ComponentHome extends Component {
     render() {
         return (
-            <div>
-                tushar
-            </div>
+            <Container>
+                <br/><br/><br/><br/>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Sr No</th>
+                            <th>Name</th>
+                            <th>Designation</th>
+                            <th>Salary</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            employees.map((emp, index)=>{
+                                return(
+                                    <tr key={index}>
+                                        <td>{ index + 1 }</td>
+                                        <td>{ emp.name }</td>
+                                        <td>{ emp.designation }</td>
+                                        <td>{ emp.salary }</td>
+                                        <td><Button href= { "/edit/"+emp.id } variant="primary">Edit</Button></td>
+                                        <td><Button href= { "/delete/"+emp.id } variant="danger">Delete</Button></td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </Container>
         )
     }
 }
---
+++ src/static_data/employee_data.js
@@ -0,0 +1,27 @@
+export const employees = [
+    {
+        id: 1,
+        name: "Tushar",
+        designation: "Software developer",
+        salary: 50000
+    },
+    {
+        id: 2,
+        name: "Akshay",
+        designation: "Business Analyst",
+        salary: 60000
+    },
+    {
+        id: 3,
+        name: "Ramesh",
+        designation: "Database Admin",
+        salary: 70000
+    },
+    {
+        id: 4,
+        name: "Sheetal",
+        designation: 'Frontend developer',
+        salary: 65000
+    }
+]
+
